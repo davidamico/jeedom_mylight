b770a8ec2d06704d416f5fcadcf7d2fa783389bc
TheDave <amico.da@gmail.com>
2026-08-14 14:40:04 +0200

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -1,1 +1,24 @@
+<?php
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+include_file('core', 'authentification', 'php');
+if (!isConnect('admin')) {
+    throw new Exception('{{401 - Accès non autorisé}}');
+}
+?>
+<form class="form-horizontal">
+    <fieldset>
+        <div class="form-group">
+            <label class="col-lg-4 control-label">{{Utilisateur MyLight (Email)}}</label>
+            <div class="col-lg-4">
+                <input class="configKey form-control" data-l1key="username" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-lg-4 control-label">{{Mot de passe MyLight}}</label>
+            <div class="col-lg-4">
+                <input type="password" class="configKey form-control" data-l1key="password" />
+            </div>
+        </div>
+    </fieldset>
+</form>
 
